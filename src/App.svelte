2c36666e056ac299d2ac12a37c015cb85ee4b242
Jungle <devex.soft@gmail.com>
2019-05-03 08:01:44 +0500

    refactor(svelte-types): Add @pyoner/svelte-types

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 <p>
   <Counter />
   <Counter value={1}>Counter 1</Counter>
-  <Counter value={100} step={3}>Counter 2</Counter>
+  <Counter value={$count} step={3}>Counter 2</Counter>
 </p>
 
 <style>
@@ -12,7 +12,15 @@
 </style>
 
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
+
   import Counter from "./Counter.svelte";
 
+  const count = writable(100);
+
   export let name: string;
+  onMount(() => {
+    console.log("App mounted");
+  });
 </script>
